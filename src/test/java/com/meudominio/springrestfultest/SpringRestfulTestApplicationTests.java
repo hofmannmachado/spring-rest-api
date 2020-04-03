501a87af5bd760ac221894a4dd09b1fddc1461a7
Marcelo Hofmann Machado <marcelo.hofmann-machado@lhind.dlh.de>
2020-04-03 18:38:15 +0200

    spring start

---
+++ src/test/java/com/meudominio/springrestfultest/SpringRestfulTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.springrestfultest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
